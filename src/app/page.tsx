9434b03e4a322698c4539266f51c598131f2dfc3
krisna dwi setyaadi <krisnadwisetyadi@gmail.com>
2023-08-24 15:17:31 +0700

    update profile picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import HomeButtonComponent from "@/components/home-button-component";
+import Image from "next/image";
+import myImage from '../../public/images/me.jpg'
 
 export default function Home() {
   return (
     <main>
       <div className="w-full flex justify-center ">
-        <div className="h-60 w-60 rounded-full bg-red-300 my-5"/>
+        <div className="h-60 w-60 rounded-full my-5">
+          <Image src={myImage} alt="my-image" className="rounded-full h-60 w-60"/>
+        </div>
       </div>
       <div className='text-center font-semibold w-full'>
         <h3 className='text-2xl'>Hi, I&apos;m Krisna!</h3>
