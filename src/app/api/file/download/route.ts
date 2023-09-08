66c947d0f950fac6c72102943a32df71b3d51b40
krisna dwi setyaadi <krisnadwisetyadi@gmail.com>
2023-09-08 21:47:21 +0700

    fix ddownload api

--- src/app/api/file/download/route.ts
+++ src/app/api/file/download/route.ts
@@ -8,13 +8,15 @@ export async function GET(req: Request) {
         const result = await query(queryString, value)
         if(result.rows[0].length === 0 ) {
           return new Response('Error data not available', { status: 400 })
-        } 
+        } else {
         const { name, content, type } = result.rows[0]
         console.log('name', name)
         const headers = new Headers()
         headers.append('Content-type', type)
         headers.append('Content-Disposition', `attachment; filename="${name}"`)
         return new Response(content, { headers, status: 200 })
+        }
+
     } catch (error) {
         console.log('error', error)
     }
