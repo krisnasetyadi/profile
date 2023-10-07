324987ac28e58b4aca39d9c505b9e71dcead9f05
krisna dwi setyaadi <krisnadwisetyadi@gmail.com>
2023-10-07 17:05:06 +0700

    fix download

--- src/app/api/file/download/route.ts
+++ src/app/api/file/download/route.ts
@@ -3,9 +3,10 @@ import { query } from '../../database/db'
 
 export async function GET(req: Request) {
         try {
-            const filename = 'CV Krisna Dwi Setyaadi'
-            const queryString = process.env.NEXT_PUBLIC_ENVIRONMENT === 'DEV' ? 'SELECT name, content, type FROM files WHERE id = $1' :  'SELECT name, content, type FROM files WHERE name = $1' ;
-            const value =  process.env.NEXT_PUBLIC_ENVIRONMENT === 'DEV' ? [6] : [filename]
+            const queryString = 'SELECT name, content, type FROM files WHERE id = $1';
+            // const value = [6]
+            // harcoded value
+            const value = [2]
             const result = await query(queryString, value)
             if(result.rows && result.rows[0].length === 0 ) {
               return new Response('Error data not available', { status: 400 })
