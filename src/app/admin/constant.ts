11ebca69bc02883efa5a0a7f16e9f1774e82bbbb
Krisna Setyaadi <krisnadwisetyadi@gmail.com>
2024-10-20 17:41:02 +0700

    update stack options

--- src/app/admin/constant.ts
+++ src/app/admin/constant.ts
@@ -1,64 +1,88 @@
 export const roleOptions = [
-    {
-        label: 'Frontend',
-        value: 'frontend',
-    },
-    {
-        label: 'Backend',
-        value: 'Backend'
-    },
-    {
-        label: 'Fullstack',
-        value: 'Fullstack'
-    }
-]
+  {
+    label: "Frontend",
+    value: "frontend",
+  },
+  {
+    label: "Backend",
+    value: "Backend",
+  },
+  {
+    label: "Fullstack",
+    value: "Fullstack",
+  },
+];
 
 export const stackOptions = [
-    {
-        label: 'React',
-        value: 'react',
-    },
-    {
-        label: 'Next JS',
-        value: 'next js'
-    },
-    {
-        label: 'Axios',
-        value: 'axios',
-    },
-    {
-        label: 'Postgre SQL',
-        value: 'postgre-sql'
-    },
-    {
-        label: 'Yup Validation',
-        value: 'yup-validation',
-    },
-    {
-        label: 'React Kendo UI',
-        value: 'react-kendo-ui'
-    },
-    {
-        label: 'React Bootstrap',
-        value: 'react-bootstrap'
-    }
-]
+  {
+    label: "React",
+    value: "react",
+  },
+  {
+    label: "Next JS",
+    value: "next js",
+  },
+  {
+    label: "Axios",
+    value: "axios",
+  },
+  {
+    label: "Postgre SQL",
+    value: "postgre-sql",
+  },
+  {
+    label: "Yup Validation",
+    value: "yup-validation",
+  },
+  {
+    label: "React Kendo UI",
+    value: "react-kendo-ui",
+  },
+  {
+    label: "React Bootstrap",
+    value: "react-bootstrap",
+  },
+  {
+    label: ".Net Core",
+    value: ".net-core",
+  },
+  {
+    label: "SQL Server",
+    value: "sql-server",
+  },
+  {
+    label: "Angular",
+    value: "angular",
+  },
+  {
+    label: "Javascript",
+    value: "javascript",
+  },
+  {
+    label: "Typescript",
+    value: "typescript",
+  },
+  {
+    label: "C#",
+    value: "csharp",
+  },
+];
 
 export const otherStackOptions = [
-    {
-        label: 'Github',
-        value: 'github',
-    },
-    {
-        label: 'Bitbucket',
-        value: 'bitbucket'
-    },
-    {
-        label: 'Swagger',
-        value: 'swagger'
-    },
-    {
-        label: 'Postman',
-        value: 'postman'
-    }
-]
+  {
+    label: "Github",
+    value: "github",
+  },
+  {
+    label: "Bitbucket",
+    value: "bitbucket",
+  },
+  {
+    label: "Swagger",
+    value: "swagger",
+  },
+  {
+    label: "Postman",
+    value: "postman",
+  },
+];
